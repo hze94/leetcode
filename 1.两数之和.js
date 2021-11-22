663f9bfb17055f6a93d5dff74dfcf3b08796eba6
书黎 <zien.hze@alibaba-inc.com>
2021-11-22 19:31:48 +0800

    feat: 两数之和.js - hash查找法

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -14,10 +14,20 @@ var twoSum = function(nums, target) {
   //   i
   // 3 2 4
   //     j
-  for (let i = 0; i < nums.length - 1; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) return [i, j];
+  // 暴力搜索 O(n)
+  // for (let i = 0; i < nums.length - 1; i++) {
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     if (nums[i] + nums[j] === target) return [i, j];
+  //   }
+  // }
+  // hash查找 O(1)
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const item = nums[i];
+    if (map.has(target - item)) {
+      return [map.get(target - item), i];
     }
+    map.set(nums[i], i);
   }
 };
 // @lc code=end
