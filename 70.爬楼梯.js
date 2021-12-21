5da7c32b08f70d734ac9315deadda2f06fb67ce1
书黎 <zien.hze@alibaba-inc.com>
2021-12-21 23:01:04 +0800

    70.爬楼梯 - 动规

---
+++ 70.爬楼梯.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=javascript
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+  // f(n) = f(n-1) + f(n-2)
+  if (n === 1) return 1;
+  let first = 1, second = 2;
+  for (let i = 3; i <= n; i++) {
+    let third = first + second;
+    first = second;
+    second = third;
+  }
+  return second;
+};
+// @lc code=end
+
